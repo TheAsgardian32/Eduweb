dc03872830cbc1aaf1ce8431fbb583ab96951bde
Farhan_Faz <113784191+TheAsgardian32@users.noreply.github.com>
2025-09-29 13:10:17 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
                 </motion.div>
                 <div className='lg:w-1/2 w-[275px]  relative'>
                     <img src={heroimg} alt="food" className=' mx-auto spin' />
-                    <div variant="outline" className='bg-red-50 gap-1 text-lg flex border-red-200 border  items-center absolute bottom-16  lg:right-14 px-4 py-2 rounded-full'>
+                    <div variant="outline" className='bg-red-50 gap-1 text-lg flex border-red-200 border  items-center absolute bottom-16 right:58 lg:right-14 px-4 py-2 rounded-full'>
                         <img src={time} alt='time' className='lg:w-12 lg:h-12 w-6' />
                         <p className='leading-4 text-sm font-semibold'>Timely Module<br /> Completion</p>
                     </div>
@@ -50,7 +50,7 @@ const Hero = () => {
 
                         </div>
                     </div>
-                    <div variant="outline" className=' flex flex-col bg-red-50 border-red-200 border  px-6 py-2 absolute top-32 left-44 lg:top-16 lg:left-24  rounded-full items-center'>
+                    <div variant="outline" className=' flex flex-col bg-red-50 border-red-200 border px-0 py-1 lg:px-6 lg:py-2 absolute top-20 left-44 lg:top-16 lg:left-24  rounded-full items-center lg:w-[150px] w-[150px]'>
                         <h1 className=' text-red-500 lg:text-2xl text-xl font-bold'>10000+ </h1>
                         <p className=' text-sm font-semibold'>IELTS Achievers </p>
                     </div>
